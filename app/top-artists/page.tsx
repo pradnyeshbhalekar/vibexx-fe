1ee37fef9c181222415221b81d3f33388b5d8bc5
Pradnyesh <pradnyeshbhalekar78@gmail.com>
2026-02-14 21:33:55 +0530

    hopefuly final fixes

--- app/top-artists/page.tsx
+++ app/top-artists/page.tsx
@@ -1,10 +1,22 @@
-import { Suspense } from "react";
-import TopArtistsClient from "./top-artists-client";
+"use client";
+
+import { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import SonicPioneersPage from "./top-artists-client";
 
 export default function TopArtistsPage() {
-  return (
-    <Suspense fallback={null}>
-      <TopArtistsClient />
-    </Suspense>
-  );
+  const params = useSearchParams();
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = params.get("token");
+
+    if (token) {
+      localStorage.setItem("jwt", token);
+      router.replace("/top-artists"); // clean URL
+    }
+  }, [params, router]);
+
+  // render your actual UI
+  return <SonicPioneersPage/>;
 }
