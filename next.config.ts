eae8d8eb2ac2f24496f276cd73a82c322df32ef2
Pradnyesh <pradnyeshbhalekar78@gmail.com>
2026-02-14 14:17:31 +0530

    added something

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
+// next.config.js
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://vibexx-be.onrender.com/api/:path*",
+      },
+    ];
+  },
 };
-
-module.exports = nextConfig;
